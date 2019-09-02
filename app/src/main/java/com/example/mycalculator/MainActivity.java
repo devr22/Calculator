97b76d439ccec2a8359653fec5af84b2f84923a8
devr22 <devr52222@gmail.com>
2019-09-02 16:20:17 +0530

    updated

--- app/src/main/java/com/example/mycalculator/MainActivity.java
+++ app/src/main/java/com/example/mycalculator/MainActivity.java
@@ -9,6 +9,9 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
+    Double num,ans;
+    char operator;
+
     private TextView textView1;
     private TextView textView2;
     private TextView textView3;
@@ -57,232 +60,107 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         btnadd.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                addition();
+                num = Double.parseDouble(textView2.getText().toString());
+                textView2.setText("");
+                textView3.setText(num + "+");
+                operator = '+';
             }
         });
 
         btnsub.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                subtraction();
+                num = Double.parseDouble(textView2.getText().toString());
+                textView2.setText("");
+                textView3.setText(num + "-");
+                operator = '-';
             }
         });
 
         btnmul.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                multiplication();
+                num = Double.parseDouble(textView2.getText().toString());
+                textView2.setText("");
+                textView3.setText(num + "*");
+                operator = '*';
             }
         });
 
         btndivide.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                division();
+                num = Double.parseDouble(textView2.getText().toString());
+                textView2.setText("");
+                textView3.setText(num + "/");
+                operator = '/';
             }
         });
 
         btnequal.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                answer();
+                arithmaticCalculation();
             }
         });
-
-        btnpercent.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                percentage();
-            }
-        });
-
-      /*  btndot.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                dotOperation();
-            }
-        });*/
 
     }
 
-    //On pressing number
+
     @Override
     public void onClick(View view) {
+        String Buttontext = ((Button)view).getText().toString();
 
-        String text = textView2.getText().toString();
-        String buttontext = ((Button)view).getText().toString();
-
-        textView2.setText(text+buttontext);
-      /*  if(text.isEmpty()){
-            textView2.setText(buttontext);
-            textView1.setText(buttontext);
-        }
-        else{
-            textView2.setText(text+buttontext);
-
-            char operator = text.charAt(0);
-
-            switch (operator){
-                case '+':
-                    add();
-                    break;
-                case '-':
-                    sub();
-                    break;
-                case '*':
-                    mul();
-                    break;
-                case '/':
-                    divide();
-                    break;
-            }
-        }*/
-
+        textView2.setText(textView2.getText() + Buttontext);
     }
 
-    private void addition(){
-
-        //String text1 = textView3.getText().toString();
-        String text2 = textView2.getText().toString();
-        String buttontext = btnadd.getText().toString();
-
-       // textView3.setText(text1+text2);
-        textView2.setText(buttontext);
-
-    }
-
-    private void subtraction(){
-
-        String text1 = textView3.getText().toString();
-        String text2 = textView2.getText().toString();
-        String buttontext = btnsub.getText().toString();
-
-        textView3.setText(text1+text2);
-        textView2.setText(buttontext);
-
-    }
-
-    private void multiplication(){
-
-        String text1 = textView3.getText().toString();
-        String text2 = textView2.getText().toString();
-        String buttontext = btnmul.getText().toString();
-
-        textView3.setText(text1+text2);
-        textView2.setText(buttontext);
-
-    }
-
-    private void division(){
-
-        String text1 = textView3.getText().toString();
-        String text2 = textView2.getText().toString();
-        String buttontext = btndivide.getText().toString();
-
-        textView3.setText(text1+text2);
-        textView2.setText(buttontext);
-
-    }
-
-    private void add(){
-        String text = textView2.getText().toString();
-        int textlength = text.length();
-
-        Float val1 = Float.parseFloat(textView3.getText().toString());
-        Float val2 = Float.parseFloat(text.substring(1,textlength-1));
-
-        Float sum = val1 + val2;
-
-        textView2.setText("="+sum.toString());
-        textView3.setText(textView3.getText()+text);
-    }
-
-    private void sub(){
-        String text = textView2.getText().toString();
-        int textlength = text.length();
-
-        Float val1 = Float.parseFloat(textView3.getText().toString());
-        Float val2 = Float.parseFloat(text.substring(1,textlength-1));
-
-        Float difference = val1 - val2;
-
-        textView2.setText("="+difference.toString());
-        textView3.setText(textView3.getText()+text);
-    }
-
-    private void mul(){
-        String text = textView2.getText().toString();
-        int textlength = text.length();
-
-        Float val1 = Float.parseFloat(textView1.getText().toString());
-        Float val2 = Float.parseFloat(text.substring(1,textlength-1));
-
-        Float product = val1 * val2;
-
-        textView2.setText("="+product.toString());
-        textView3.setText(textView3.getText()+text);
-    }
-
-    private void divide(){
-        String text = textView2.getText().toString();
-        int textlength = text.length();
-
-        Float val1 = Float.parseFloat(textView1.getText().toString());
-        Float val2 = Float.parseFloat(text.substring(1,textlength-1));
-
-        Float div = val1 / val2;
-
-        textView2.setText("="+div.toString());
-        textView3.setText(textView3.getText()+text);
-    }
-
-    private void answer(){
-        String text = textView2.getText().toString();
-        char operator = text.charAt(0);
+    private void arithmaticCalculation(){
 
         switch (operator){
             case '+':
                 add();
                 break;
+
             case '-':
                 sub();
                 break;
+
             case '*':
                 mul();
                 break;
+
             case '/':
-                divide();
+                div();
                 break;
         }
 
-       /* textView3.setText(textView3.getText()+text);
-        textView2.setText(textView1.getText().toString());
-        textView1.setText("");*/
     }
 
-    private void percentage(){
-        String text = textView2.getText().toString();
+    private void add(){
+        ans = num + Double.parseDouble(textView2.getText().toString());
 
-        if (text.contains("+")||text.contains("-")||text.contains("*")||text.contains("/")||text.isEmpty()){
-
-        }
-        else{
-            Float val = Float.parseFloat(text);
-            Float percent = val/100;
-            textView2.setText(percent.toString());
-        }
+        textView3.setText(textView3.getText() + textView2.getText().toString());
+        textView2.setText(Double.toString(ans));
     }
 
-  /* private void dotOperation(){
-        String text = textView2.getText().toString();
-        String buttontext = btndot.getText().toString();
+    private void sub(){
+        ans = num - Double.parseDouble(textView2.getText().toString());
 
-        if (text.isEmpty()){
-            textView2.setText("0.");
-        }
-        else if (text.equals("+") || text.equals("-") || text.equals("*") || text.equals("/")){
-            textView2.setText(text+"0.");
-        }
-    }*/
+        textView3.setText(textView3.getText() + textView2.getText().toString());
+        textView2.setText(Double.toString(ans));
+    }
 
+    private void mul(){
+        ans = num * Double.parseDouble(textView2.getText().toString());
+
+        textView3.setText(textView3.getText() + textView2.getText().toString());
+        textView2.setText(Double.toString(ans));
+    }
+
+    private void div(){
+        ans = num / Double.parseDouble(textView2.getText().toString());
+
+        textView3.setText(textView3.getText() + textView2.getText().toString());
+        textView2.setText(Double.toString(ans));
+    }
 }
